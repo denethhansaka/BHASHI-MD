2363043038df72530fecbb66887d1debd3f5cfe5
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-09 03:47:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ module.exports = {
   ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "false",
   ANTI_BOT: process.env.ANTI_BOT || "true",
   ANTI_DELETE: process.env.ANTI_DELETE || "true",
-  packname: process.env.packname || "🪄BHASHI",
-  author: process.env.author || "BHASHI x VISHWA",
+  packname: process.env.packname || "DENETH-MD",
+  author: process.env.author || "DENETH-MD",
   //==========================================- API-CONFIGS -==========================================================
   OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || "2d61a72574c11c4f36173b627f8cb177", //openweathermap.org
   ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || "sk_6438bcc100d96458f8de0602aec662f4ba14b905fd090ad3", //elevenlabs.io
